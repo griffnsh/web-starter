6ca8f614afe13ad6c91229f6317d8bdb1ca66595
griffnsh <memvivexp@gmail.com>
2024-05-02 13:22:36 -0500

    added dark mode toggle svg & styling

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const burger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 const navList = document.querySelector(".nav-list");
 const burgerBar = document.querySelectorAll(".hamburger-bar");
+const header = document.querySelector(".header")
 
 burger.addEventListener("click", toggle => {
   nav.classList.toggle("active");
@@ -9,12 +10,21 @@ burger.addEventListener("click", toggle => {
   burgerBar.classList.toggle("bar-toggle")
 })
 
-// burgerBar.addEventListener("click", () => {
-//   console.log("test");
-// })
-
 function toggleHamburger() {
  burgerBar.forEach(burgerBar => burgerBar.classList.toggle('hamburger-bar-toggle'))
 }
 
 burger.addEventListener('click', toggleHamburger)
+
+// Sticky Nav
+window.onscroll = function() {stickyNav()};
+
+const sticky = header.offsetTop;
+
+function stickyNav() {
+  if (window.scrollY >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
